8abde27e4f4b8124789becdf174208a97518e6af
Nat Budin <natbudin@gmail.com>
2019-10-17 11:00:22 -0700

    Don't break trying to render nil as markdown

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -3,6 +3,8 @@ module Types
     field_class Types::BaseField
 
     def render_markdown(text)
+      return '' unless text
+
       markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new)
       markdown.render(text).html_safe
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def render_markdown(text)
+    return '' unless text
+
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new)
     markdown.render(text).html_safe
   end
